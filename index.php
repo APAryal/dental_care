eb9b1f73e705294b6f5ea951149fbd17ca99b058
Apsara Aryal <100707766+APAryal@users.noreply.github.com>
2024-05-04 23:33:45 +0545

    Update index.php

--- index.php
+++ index.php
@@ -220,24 +220,24 @@ $message[] ='appointment failed';
  <div class="box">
  <i class="fas fa-phone"> </i>
  <h3> phone number</h3>
- <p>+123-456-7890</p>
- <p>+111-222-3333</p>
+ <p>9822111111</p>
+ <p>9849000000</p>
  </div>
  <div class="box">
  <i class="fas fa-map-marker-alt"></i>
  <h3>our address</h3>
- <p>mumbai, india - 400104</p>
+ <p>nepal, kathmandu - 566669</p>
  </div>
   <div class="box">
  <i class="fas fa-clock"></i>
  <h3>opening hours</h3>
- <p>00:07am to 10:00pm</p>
+ <p>00:08am to 10:00pm</p>
  </div>
   <div class="box">
  <i class="fas fa-envelope"> </i>
  <h3> email address</h3>
- <p>shaikhanas@gmail.com</p>
- <p>ahaikhanas@gmail.com</p>
+ <p>apsara@gmail.com</p>
+ <p>ap@gmail.com</p>
  <p>+111-222-3333</p>
 
  </div>
